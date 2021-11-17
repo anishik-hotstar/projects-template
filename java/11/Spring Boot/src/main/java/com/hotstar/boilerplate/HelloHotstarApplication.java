fb832f4544cfef003254a036ad44c4013288db9d
Apoorva Jain <apoorvajain@hotstar.com>
2021-11-17 20:15:54 +0530

    gradle added

--- java/11/Spring Boot/src/main/java/com/hotstar/boilerplate/HelloHotstarApplication.java
+++ java/11/Spring Boot/src/main/java/com/hotstar/boilerplate/HelloHotstarApplication.java
@@ -2,28 +2,27 @@ package com.hotstar.boilerplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
-
 import java.util.HashMap;
 import java.util.Map;
 
-@RestController
+
 @SpringBootApplication
+@RestController
 public class HelloHotstarApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(HelloHotstarApplication.class, args);
     }
-
-     @GetMapping("/healthy")
+    @GetMapping("/healthy")
     public ResponseEntity<Map<String, String>> healthCheck() {
-         Map<String, String> response = new HashMap<>();
-         response.put("status", "Welcome to hello! hotstar bootcamp");
-         return ResponseEntity.ok().body(response);
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "Welcome to hello! hotstar bootcamp yeah");
+        return ResponseEntity.ok().body(response);
     }
 
-
 }
-
 
