91f41b30d9c0dba962c12aa8ab95710ff11baa36
Apoorva Jain <apoorvajain@hotstar.com>
2021-11-17 13:09:28 +0530

    gradle added

--- java/11/Spring Boot/src/main/java/com/hotstar/boilerplate/HelloHotstarApplication.java
+++ java/11/Spring Boot/src/main/java/com/hotstar/boilerplate/HelloHotstarApplication.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @SpringBootApplication
 public class HelloHotstarApplication {
