14afd7bded20f58f767e6718a290f0efd2184550
amarnath-hotstar <52204327+amarnath-hotstar@users.noreply.github.com>
2022-01-07 14:45:29 +0530

    Update hello-world.go

--- Go/1/basic/src/main/go/com/hotstar/boilerplate/hello-world.go
+++ Go/1/basic/src/main/go/com/hotstar/boilerplate/hello-world.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
 )
 
-func main() {
-    http.HandleFunc("/healthy", HelloServer)
-    http.ListenAndServe(":8080", nil)
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, world!!!")
 }
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, world!!!")
+func main() {
+	http.HandleFunc("/healthy", HelloServer)
+	http.ListenAndServe(":8080", nil)
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":4001", nil)
 }
